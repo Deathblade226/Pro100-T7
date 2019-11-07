ec9ba6e7cacd9ac655e5ffeb988efc3f2608fbda
Andrew Jensen <AJensen@student.neumont.edu>
2019-11-07 14:00:53 -0700

    Added History class

--- Pro100-T7/Model/History.cs
+++ Pro100-T7/Model/History.cs
@@ -4,9 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Pro100_T7.Model
 {
-    class History
+	public class Action
+	{
+		public int layerID;
+		public Layers layer;
+	}
+    public static class History
     {
+		//one way to 
+		private static Stack<Action> undoStack = new Stack<Action>();
+		private static Stack<Action> redoStack = new Stack<Action>();
+
+		//puts a clear default action at the start of the undo stack so we can undo the first action taken by the user. Call this to initialize the class
+		public static void StartHistory()
+		{
+			undoStack.Push(new Action());
+		}
+		
+		public static void EndAction(Action a)
+		{
+			if(redoStack.Count > 0)
+			{
+				redoStack.Clear();
+			}
+			undoStack.Push(a);
+		}
+
+		public static void Undo()
+		{
+			if(undoStack.Count > 0)
+			{
+				Action undoneAction = undoStack.Pop();
+				redoStack.Push(undoneAction);
+			}
+
+		}
+
+		public static void Redo()
+		{
+			if(redoStack.Count > 0)
+			{
+				Action redoneAction = redoStack.Pop();
+				undoStack.Push(redoneAction);
+			}
+		}
+
     }
 }
