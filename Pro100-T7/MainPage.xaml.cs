bc5a52786dc6fadabc79f6f38080d2b0caff9e6f
Joshua Favre <jfavre390@gmail.com>
2019-11-07 14:09:43 -0700

    Formatted files and removed whitespace
    No code change, formatted some documents and removed excess whitespace

--- Pro100-T7/MainPage.xaml.cs
+++ Pro100-T7/MainPage.xaml.cs
@@ -21,61 +21,61 @@ using Windows.UI.ViewManagement;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Pro100_T7 {
+namespace Pro100_T7
+{
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-public sealed partial class MainPage : Page {
+    public sealed partial class MainPage : Page
+    {
 
-WriteableBitmap bmp = BitmapFactory.New(1500, 800);
-Point current = new Point();
-Point old = new Point();
-List<Image> images = new List<Image>();
+        WriteableBitmap bmp = BitmapFactory.New(1500, 800);
+        Point current = new Point();
+        Point old = new Point();
+        List<Image> images = new List<Image>();
 
-public MainPage() {
-    this.InitializeComponent();
-    ApplicationView.PreferredLaunchViewSize = new Size(1500, 1000);
-    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
-}
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.PreferredLaunchViewSize = new Size(1500, 1000);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
 
-protected override async void OnNavigatedTo(NavigationEventArgs e) {
-    base.OnNavigatedTo(e);
-    PointerMoved += MainPage_PointerMoved;
-}
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            PointerMoved += MainPage_PointerMoved;
+        }
 
-private void MainPage_PointerMoved(object sender, PointerRoutedEventArgs e) {
-    Pointer ptr = e.Pointer;
-    if (ptr.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse) {
-    Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(null);
-    current = e.GetCurrentPoint(null).Position;
-    if (ptrPt.Properties.IsLeftButtonPressed) {
-    Mouse.Text = "x:" + e.GetCurrentPoint(null).Position.X.ToString() + " | y:" + e.GetCurrentPoint(null).Position.Y.ToString();
+        private void MainPage_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Pointer ptr = e.Pointer;
+            if (ptr.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse)
+            {
+                Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(null);
+                current = e.GetCurrentPoint(null).Position;
+                if (ptrPt.Properties.IsLeftButtonPressed)
+                {
+                    Mouse.Text = "x:" + e.GetCurrentPoint(null).Position.X.ToString() + " | y:" + e.GetCurrentPoint(null).Position.Y.ToString();
 
-    Image.Source = bmp;
-    using (bmp.GetBitmapContext()) {
+                    Image.Source = bmp;
+                    using (bmp.GetBitmapContext())
+                    {
+                        //bmp.SetPixel((int)current.X, (int)current.Y, Colors.Black);
+                        Rect size = new Rect();
+                        bmp.DrawLineAa((int)old.X, (int)old.Y, (int)current.X, (int)current.Y, Colors.Black, 5);
+                    }
+                    old = e.GetCurrentPoint(null).Position;
+                }
+                else { old = current; }
 
-    //bmp.SetPixel((int)current.X, (int)current.Y, Colors.Black);
+                if (ptrPt.Properties.IsRightButtonPressed)
+                {
 
-    Rect size = new Rect();
-    bmp.DrawLineAa((int)old.X, (int)old.Y, (int)current.X, (int)current.Y, Colors.Black, 5);
-
-
+                }
+            }
+        }
     }
-    old = e.GetCurrentPoint(null).Position;
-
-    
-    }else { old = current;}
-
-    if (ptrPt.Properties.IsRightButtonPressed) {
-    
-    }
-
-    }
-    
-    }
-    
-
-}
 
 }
 
