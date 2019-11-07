bf62e8cfd69f4caf85770f40184d70d8b3f72c09
Jason Clarke <jclarke@student.neumont.edu>
2019-11-07 14:53:47 -0700

    Update

--- Pro100-T7/MainPage.xaml.cs
+++ Pro100-T7/MainPage.xaml.cs
@@ -12,19 +12,70 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.ViewManagement;
+
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Pro100_T7
-{
+namespace Pro100_T7 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
-    {
-        public MainPage()
-        {
-            this.InitializeComponent();
-        }
-    }
+public sealed partial class MainPage : Page {
+
+WriteableBitmap bmp = BitmapFactory.New(1500, 800);
+Point current = new Point();
+Point old = new Point();
+List<Image> images = new List<Image>();
+
+public MainPage() {
+    this.InitializeComponent();
+    ApplicationView.PreferredLaunchViewSize = new Size(1500, 1000);
+    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
 }
+
+protected override async void OnNavigatedTo(NavigationEventArgs e) {
+    base.OnNavigatedTo(e);
+    PointerMoved += MainPage_PointerMoved;
+}
+
+private void MainPage_PointerMoved(object sender, PointerRoutedEventArgs e) {
+    Pointer ptr = e.Pointer;
+    if (ptr.PointerDeviceType == Windows.Devices.Input.PointerDeviceType.Mouse) {
+    Windows.UI.Input.PointerPoint ptrPt = e.GetCurrentPoint(null);
+    current = e.GetCurrentPoint(null).Position;
+    if (ptrPt.Properties.IsLeftButtonPressed) {
+    Mouse.Text = "x:" + e.GetCurrentPoint(null).Position.X.ToString() + " | y:" + e.GetCurrentPoint(null).Position.Y.ToString();
+
+    Image.Source = bmp;
+    using (bmp.GetBitmapContext()) {
+    
+    //bmp.DrawLineAa((int)old.X, (int)old.Y, (int)current.X, (int)current.Y, colorPicker.Color, 100);
+
+    WriteableBitmapExtensions.DrawLine(bmp, (int)old.X, (int)old.Y, (int)current.X, (int)current.Y, colorPicker.Color);
+
+    }
+    old = e.GetCurrentPoint(null).Position;
+
+    
+    }else { old = current;}
+
+    //if (ptrPt.Properties.IsRightButtonPressed) {}
+
+    //bmp.Clear(Colors.White);
+    
+    }
+
+    }
+    
+    }
+    
+
+}
+
+
+
