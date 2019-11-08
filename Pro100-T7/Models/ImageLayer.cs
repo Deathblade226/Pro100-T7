73562e132f40898f47e7cd6c86f76dd1d17b3ab4
Joshua Favre <jfavre390@gmail.com>
2019-11-08 14:08:16 -0700

    ImageLayer Creation

---
+++ Pro100-T7/Models/ImageLayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro100_T7.Models
+{
+    public class ImageLayer
+    {
+
+    }
+}
