17419aab21734be8034bae7e33f2e65834b85205
Andrew Jensen <AJensen@student.neumont.edu>
2019-11-12 15:20:52 -0700

    Fixed the redo bug

--- Pro100-T7/Models/History.cs
+++ Pro100-T7/Models/History.cs
@@ -20,51 +20,65 @@ namespace Pro100_T7.Models
 	}
     public static class History
     {
-		private static Stack<Action> undoStack = new Stack<Action>();
+		private static Stack<Action> historyStack = new Stack<Action>();
 		private static Stack<Action> redoStack = new Stack<Action>();
 
 		//puts a clear default action at the start of the undo stack so we can undo the first action taken by the user. Call this to initialize the class
+		/// <summary>
+		/// Initializes the class. Must be called when the image is loaded before any actions are taken
+		/// </summary>
+		/// <param name="currentState">Represents the current state of the image on loading.</param>
 		public static void StartHistory(byte[] currentState)
 		{
 			Action temp = new Action(currentState);
-			undoStack.Push(temp);
+			historyStack.Push(temp);
 			redoStack.Push(temp);
 		}
 		
+		/// <summary>
+		/// Takes in the state of the image to be recorded in the history stack.
+		/// </summary>
+		/// <param name="a">An action holding a byte array that represents the state of the image AFTER the action is taken.</param>
 		public static void EndAction(Action a)
 		{
 			while(redoStack.Count > 1)
 			{
 				redoStack.Pop();
 			}
-			undoStack.Push(a);
+			historyStack.Push(a);
 		}
 
 		//will return an ImageLayer or an action depends on how ImageLayer is implemented
-		//returns the image how it should look AFTER the undo is executed
+		/// <summary>
+		/// Undoes last taken action. 
+		/// </summary>
+		/// <returns>Returns an Action hold the byte array representing the state AFTER the undo is executed.</returns>
 		public static Action Undo()
 		{
-			if(undoStack.Count > 1)
+			if(historyStack.Count > 1)
 			{
-				Action undoneAction = undoStack.Pop();
+				Action undoneAction = historyStack.Pop();
 				redoStack.Push(undoneAction);
 			}
 
-			Action a = undoStack.Peek();
+			Action a = historyStack.Peek();
 			return a;
 
 		}
 
-		//Not finished 
+		/// <summary>
+		/// Redoes last undone action. Clears after a new action is taken.
+		/// </summary>
+		/// <returns>Returns an Action holding the byte array representing the state AFTER the redo is executed.</returns>
 		public static Action Redo()
 		{
 			if(redoStack.Count > 1)
 			{
 				Action redoneAction = redoStack.Pop();
-				undoStack.Push(redoneAction);
+				historyStack.Push(redoneAction);
 			}
 
-			Action a = redoStack.Peek();
+			Action a = historyStack.Peek();
 			return a;
 		}
 
