d1ff5e6c89b769f727b63eb5dbdb4a943d60f928
Andrew Jensen <AJensen@student.neumont.edu>
2019-11-17 10:28:11 -0700

    Add FillTool and BitmapReaderClass
    BitmapReader is used as a replacement for the current GetPixel and SetPixel methods, as they are horribly slow. FillTool uses the scanline algorithm to flood fill and just needs to be connected to the UI to be used.

---
+++ Pro100-T7/Models/BitmapReader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Pro100_T7.Models
+{
+	public class BitmapReader : IDisposable
+	{
+		public int width;
+		public int height;
+		WriteableBitmap bmp;
+		public byte[] bytes;
+
+		public BitmapReader(WriteableBitmap bmp)
+		{
+			this.bmp = bmp;
+			this.width = bmp.PixelWidth;
+			this.height = bmp.PixelHeight;
+			this.bytes = bmp.PixelBuffer.ToArray();
+		}
+
+		public Color GetPixel(int x, int y)
+		{
+			int i = ((y * width) + x) * sizeof(int);
+			byte b = bytes[i];
+			i++;
+			byte g = bytes[i];
+			i++;
+			byte r = bytes[i];
+			i++;
+			byte a = bytes[i];
+
+			return Color.FromArgb(a, r, g, b);
+		}
+
+		public int GetPixeli(int x, int y)
+		{
+			int i = ((y * width) + x) * sizeof(int);
+			byte b = bytes[i];
+			i++;
+			byte g = bytes[i];
+			i++;
+			byte r = bytes[i];
+			i++;
+			byte a = bytes[i];
+
+
+			int color = ((a << 24) | (r << 16) | (g << 8) | b);
+			return color;
+		}
+
+		public void SetPixel(int x, int y, int color)
+		{
+			int i = ((y * width) + x) * sizeof(int);
+			bytes[i] = (byte)(color >> 0);
+			i++;
+			bytes[i] = (byte)(color >> 8);
+			i++;
+			bytes[i] = (byte)(color >> 16);
+			i++;
+			bytes[i] = (byte)(color >> 24);
+
+			bmp.Invalidate();
+		}
+
+
+		public void Dispose()
+		{
+			bmp.PixelBuffer.AsStream().Write(bytes, 0, bytes.Length);
+		}
+	}
+}
---
+++ Pro100-T7/Models/FillTool.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Pro100_T7.Models
+{
+	public class FillTool
+	{
+		public static int toFillWith = (((byte)255) << 24 ) | (((byte)255) << 16);
+		public static int toBeFilled;
+
+		public static Queue<P> points = new Queue<P>();
+
+		public static BitmapReader bmpReader;
+
+		public struct P
+		{
+			public int x;
+			public int y;
+
+			public P(int x, int y)
+			{
+				this.x = x;
+				this.y = y;
+			}
+		}
+
+
+		public static void Fill(WriteableBitmap bitmap, int x, int y)
+		{
+
+			using (bmpReader = new BitmapReader(bitmap))
+			{
+				toBeFilled = bmpReader.GetPixeli(x, y);
+				if (toBeFilled != toFillWith)
+				{
+					FloodFillScanlineV2(x, y);
+				}
+
+			}
+
+		}
+
+		
+
+		/// <summary>
+		/// Uses the scanline algorithm to flood fill a selected area, starting from the point given.
+		/// </summary>
+		/// <param name="x">The X position to start the fill from</param>
+		/// <param name="y">The Y position to start the fill from</param>
+		private static void FloodFillScanlineV2(int x, int y)
+		{
+			points.Enqueue(new P(x, y));
+
+			while (points.Count != 0)
+			{
+				P temp = points.Dequeue();
+				int y1 = temp.y;
+
+
+				while (y1 >= 0 && bmpReader.GetPixeli(temp.x, y1) == toBeFilled)
+				{
+					y1--;
+				}
+				y1++;
+				bool spanLeft = false;
+				bool spanRight = false;
+				while (y1 < bmpReader.height && bmpReader.GetPixeli(temp.x, y1) == toBeFilled)
+				{
+					bmpReader.SetPixel(temp.x, y1, toFillWith);
+
+					if (!spanLeft && temp.x > 0 && bmpReader.GetPixeli(temp.x - 1, y1) == toBeFilled)
+					{
+						points.Enqueue(new P(temp.x - 1, y1));
+						spanLeft = true;
+					}
+					else if (spanLeft || temp.x - 1 == 0 && bmpReader.GetPixeli(temp.x - 1, y1) != toBeFilled)
+					{
+						spanLeft = false;
+					}
+					if (!spanRight && temp.x < bmpReader.width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) == toBeFilled)
+					{
+						points.Enqueue(new P(temp.x + 1, y1));
+						spanRight = true;
+					}
+					else if (spanRight && temp.x < bmpReader.width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) != toBeFilled)
+					{
+						spanRight = false;
+					}
+					y1++;
+				}
+			}
+		}
+
+
+	}
+}
