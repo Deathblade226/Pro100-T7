243d904ebf951cb388db262f84857c377da39286
Andrew Jensen <AJensen@student.neumont.edu>
2019-11-19 14:34:43 -0700

    Encapsulate BitmapReader and FillTool and add documentation

--- Pro100-T7/Models/BitmapReader.cs
+++ Pro100-T7/Models/BitmapReader.cs
@@ -9,24 +9,33 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace Pro100_T7.Models
 {
+	/// <summary>
+	/// Encapsulates a WriteableBitmap to allow for fast GetPixel and SetPixel methods. Implements IDisposable, and changes are not saved to the bitmap until after the reader is disposed of.
+	/// </summary>
 	public class BitmapReader : IDisposable
 	{
-		public int width;
-		public int height;
-		WriteableBitmap bmp;
-		public byte[] bytes;
+		public int Width { get; private set; }
+		public int Height { get; private set; }
+		private WriteableBitmap bmp;
+		private byte[] bytes;
 
 		public BitmapReader(WriteableBitmap bmp)
 		{
 			this.bmp = bmp;
-			this.width = bmp.PixelWidth;
-			this.height = bmp.PixelHeight;
+			this.Width = bmp.PixelWidth;
+			this.Height = bmp.PixelHeight;
 			this.bytes = bmp.PixelBuffer.ToArray();
 		}
 
+		/// <summary>
+		/// Returns the color at the specified coordinates from the bitmap used to initialize this class.
+		/// </summary>
+		/// <param name="x">The x coordinate of the pixel</param>
+		/// <param name="y">The y coordinate of the pixel</param>
+		/// <returns>The color at the specified location as a Color object</returns>
 		public Color GetPixel(int x, int y)
 		{
-			int i = ((y * width) + x) * sizeof(int);
+			int i = ((y * Width) + x) * sizeof(int);
 			byte b = bytes[i];
 			i++;
 			byte g = bytes[i];
@@ -38,9 +47,15 @@ namespace Pro100_T7.Models
 			return Color.FromArgb(a, r, g, b);
 		}
 
+		/// <summary>
+		/// Returns the color at the specified coordinates from the bitmap used to initialize this class.
+		/// </summary>
+		/// <param name="x">The x coordinate of the pixel</param>
+		/// <param name="y">The y coordinate of the pixel</param>
+		/// <returns>An integer representation of the color (stored as ARGB)</returns>
 		public int GetPixeli(int x, int y)
 		{
-			int i = ((y * width) + x) * sizeof(int);
+			int i = ((y * Width) + x) * sizeof(int);
 			byte b = bytes[i];
 			i++;
 			byte g = bytes[i];
@@ -54,9 +69,15 @@ namespace Pro100_T7.Models
 			return color;
 		}
 
+		/// <summary>
+		/// Sets the pixel at the specified location of the bitmap used to initialize this class.
+		/// </summary>
+		/// <param name="x">The x coordinate of the pointer</param>
+		/// <param name="y">The y coordinate of the pointer</param>
+		/// <param name="color">The desired color as a Color object</param>
 		public void SetPixel(int x, int y, Color color)
 		{
-			int i = ((y * width) + x) * sizeof(int);
+			int i = ((y * Width) + x) * sizeof(int);
 			bytes[i] = (byte)(color.B);
 			i++;
 			bytes[i] = (byte)(color.G);
@@ -67,9 +88,15 @@ namespace Pro100_T7.Models
 
 		}
 
+		/// <summary>
+		/// Sets the pixel at the specified location of the bitmap used to initialize this class.
+		/// </summary>
+		/// <param name="x">The x coordinate of the pointer</param>
+		/// <param name="y">The y coordinate of the pointer</param>
+		/// <param name="color">The desired color as an integer (stored as ARGB)</param>
 		public void SetPixel(int x, int y, int color)
 		{
-			int i = ((y * width) + x) * sizeof(int);
+			int i = ((y * Width) + x) * sizeof(int);
 			bytes[i] = (byte)(color >> 0);
 			i++;
 			bytes[i] = (byte)(color >> 8);
@@ -77,11 +104,12 @@ namespace Pro100_T7.Models
 			bytes[i] = (byte)(color >> 16);
 			i++;
 			bytes[i] = (byte)(color >> 24);
-
-			//bmp.Invalidate();
 		}
 
 
+		/// <summary>
+		/// This method should be automatically called if the instance is created with the using keyword. Call this method to save the changes to the bitmap passed it.
+		/// </summary>
 		public void Dispose()
 		{
 			bmp.PixelBuffer.AsStream().Write(bytes, 0, bytes.Length);
--- Pro100-T7/Models/FillTool.cs
+++ Pro100-T7/Models/FillTool.cs
@@ -10,16 +10,15 @@ using Windows.UI.Xaml.Media.Imaging;
 
 namespace Pro100_T7.Models
 {
-	public class FillTool
+	public static class FillTool
 	{
-		public static int toFillWith;
-		public static int toBeFilled;
+		private static int toFillWith;
+		private static int toBeFilled;
 
-		public static Queue<P> points = new Queue<P>();
-
-		public static BitmapReader bmpReader;
-
-		public struct P
+		/// <summary>
+		/// Simple struct to represent a point, only used in this class.
+		/// </summary>
+		private struct P
 		{
 			public int x;
 			public int y;
@@ -31,22 +30,36 @@ namespace Pro100_T7.Models
 			}
 		}
 
-
+		/// <summary>
+		/// Edits the passed in bitmap and fills the area surrounding the point with the color passed in
+		/// </summary>
+		/// <param name="bitmap">The bitmap to be edited</param>
+		/// <param name="x">The x value of the pointer location</param>
+		/// <param name="y">The y value of the pointer location</param>
+		/// <param name="color">The color to fill the area with</param>
 		public static void Fill(WriteableBitmap bitmap, int x, int y, Color color)
 		{
 			int intColor = ((color.A << 24) | (color.R << 16) | (color.G << 8) | color.B);
 			Fill(bitmap, x, y, intColor);
 		}
 
+		/// <summary>
+		/// Fills the area surrounding the point with the color passed in
+		/// </summary>
+		/// <param name="bitmap">The bitmap to be edited</param>
+		/// <param name="x">The x value of the pointer location</param>
+		/// <param name="y">The y value of the pointer location</param>
+		/// <param name="color">The color to fill the area with</param>
 		public static void Fill(WriteableBitmap bitmap, int x, int y, int color)
 		{
 			toFillWith = color;
-			using (bmpReader = new BitmapReader(bitmap))
+			//use the using keyword to ensure the BitmapReader is disposed of before the method returns.
+			using (BitmapReader bmpReader = new BitmapReader(bitmap))
 			{
 				toBeFilled = bmpReader.GetPixeli(x, y);
 				if (toBeFilled != toFillWith)
 				{
-					FloodFillScanlineV2(x, y);
+					FloodFillScanline(bmpReader, x, y);
 				}
 
 			}
@@ -60,8 +73,10 @@ namespace Pro100_T7.Models
 		/// </summary>
 		/// <param name="x">The X position to start the fill from</param>
 		/// <param name="y">The Y position to start the fill from</param>
-		private static void FloodFillScanlineV2(int x, int y)
+		private static void FloodFillScanline(BitmapReader bmpReader, int x, int y)
 		{
+			Queue<P> points = new Queue<P>();
+
 			points.Enqueue(new P(x, y));
 
 			while (points.Count != 0)
@@ -77,7 +92,7 @@ namespace Pro100_T7.Models
 				y1++;
 				bool spanLeft = false;
 				bool spanRight = false;
-				while (y1 < bmpReader.height && bmpReader.GetPixeli(temp.x, y1) == toBeFilled)
+				while (y1 < bmpReader.Height && bmpReader.GetPixeli(temp.x, y1) == toBeFilled)
 				{
 					bmpReader.SetPixel(temp.x, y1, toFillWith);
 
@@ -90,12 +105,12 @@ namespace Pro100_T7.Models
 					{
 						spanLeft = false;
 					}
-					if (!spanRight && temp.x < bmpReader.width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) == toBeFilled)
+					if (!spanRight && temp.x < bmpReader.Width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) == toBeFilled)
 					{
 						points.Enqueue(new P(temp.x + 1, y1));
 						spanRight = true;
 					}
-					else if (spanRight && temp.x < bmpReader.width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) != toBeFilled)
+					else if (spanRight && temp.x < bmpReader.Width - 1 && bmpReader.GetPixeli(temp.x + 1, y1) != toBeFilled)
 					{
 						spanRight = false;
 					}
