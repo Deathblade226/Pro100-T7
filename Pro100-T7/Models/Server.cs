3c81ab0a6a38da199535812a8a5d7c204cccf6fd
Joshua Favre <jfavre390@gmail.com>
2019-11-21 15:04:47 -0700

    Connection backbone organized
    No functionality

--- Pro100-T7/Models/Server.cs
+++ Pro100-T7/Models/Server.cs
@@ -2,13 +2,30 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pro100_T7.Models
 {
-    public sealed class Server : IClientServer
+    public sealed class Server : IClientServer, IServer
     {
+        public Stack<Socket> Clients { get; private set; } = new Stack<Socket>();
+
+        public Server()
+        {
+
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendData()
+        {
+            throw new NotImplementedException();
+        }
 
     }
 }
