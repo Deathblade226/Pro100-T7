30809edb2cb36cc524ebf140d205dfc4392aa799
Joshua Favre <jfavre390@gmail.com>
2019-11-26 14:20:08 -0700

    Helper (sockets suck ass)

--- NetworkingTests/Program.cs
+++ NetworkingTests/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace NetworkingTests
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IPEndPoint local = new IPEndPoint(IPAddress.Any, 5555);
+            Socket listener = new Socket(local.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(local);
+            listener.Listen(1);
+
+            Socket accepted = listener.Accept();
         }
     }
 }
