a061e40f8c959bfaf90bf219439ada888199042d
Joshua Favre <jfavre390@gmail.com>
2019-11-26 14:01:07 -0700

    Networking test poject

---
+++ NetworkingTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetworkingTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
